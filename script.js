d71969ed7747c284d9296117bbb27b73a4ab27d6
kimwaki <moreenkim36@gmail.com>
2024-08-14 10:08:13 +0300

    add the validity check

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('registration-form');
     const feedbackDiv = document.getElementById('form-feedback')
+
+    const isValid = true;
+
+    const messages = ["this is an error", "try again"];
+
+
     form.addEventListener('submit', function(event) {
         event.preventDefault();
 
@@ -16,6 +22,13 @@ document.addEventListener('DOMContentLoaded', function() {
             errorMessage.textContent = '';
             form.submit();
         }
+        if (usernameInputField.value.length < 3) {
+            isValid = false;
+            usernameError.textContent = 'Username must be at least 3 characters long.';
+            return;
+          } else {
+            usernameError.textContent = '';
+          }
 
         if (emailInputField.value.trim() === '') {
             errorMessage.textContent = 'This field is required.';
